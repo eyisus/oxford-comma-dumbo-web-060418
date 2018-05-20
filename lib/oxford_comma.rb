569b7701bac63a09ccad95b2594fb61b6cb3ae86
Jesus Escalona <eyisus@gmail.com>
2018-05-20 16:17:44 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+case array.size
+when 1
+  return array.join
+when 2
+  return array.join(" and ")
+else
+  if array.size>2
+  newArr = array.pop
+  form = array.join(", ")
+  return form << ", and #{newArr}"
+end
+end
 end
